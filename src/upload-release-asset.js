9bda69936066a7d6be50c0926648a2a9f2e6a3e3
LI Tao <litroncn@gmail.com>
2020-02-02 20:03:52 +0800

    update

--- src/upload-release-asset.js
+++ src/upload-release-asset.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
-const { GitHub } = require('@actions/github');
+const { GitHub, context } = require('@actions/github');
+
 const fs = require('fs');
 
 async function run() {
@@ -7,11 +8,20 @@ async function run() {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
     const github = new GitHub(process.env.GITHUB_TOKEN);
 
+    // Get owner and repo from context of payload that triggered the action
+    const { owner, repo } = context.repo;
+
+    // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
+    const tagName = core.getInput('tag_name', { required: true });
+
+    // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.10.15' to 'v1.10.15'
+    const tag = tagName.replace('refs/tags/', '');
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
     const uploadUrl = core.getInput('upload_url', { required: true });
     const assetPath = core.getInput('asset_path', { required: true });
     const assetName = core.getInput('asset_name', { required: true });
     const assetContentType = core.getInput('asset_content_type', { required: true });
+    let uploadUrl2;
 
     // Determine content-length for header to upload asset
     const contentLength = filePath => fs.statSync(filePath).size;
@@ -19,11 +29,28 @@ async function run() {
     // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset for more information
     const headers = { 'content-type': assetContentType, 'content-length': contentLength(assetPath) };
 
+    try {
+      // First, try to get the release, which will only work if it's already published.
+      const getReleaseByTagResponse = await github.repos.getReleaseByTag({
+        owner,
+        repo,
+        tag
+      });
+
+      if (getReleaseByTagResponse && getReleaseByTagResponse.data) {
+        uploadUrl2 = getReleaseByTagResponse.data.upload_url;
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+
+    console.log(uploadUrl);
+
     // Upload a release asset
     // API Documentation: https://developer.github.com/v3/repos/releases/#upload-a-release-asset
     // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-upload-release-asset
     const uploadAssetResponse = await github.repos.uploadReleaseAsset({
-      url: uploadUrl,
+      url: uploadUrl2,
       headers,
       name: assetName,
       file: fs.readFileSync(assetPath)
